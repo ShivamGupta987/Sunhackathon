e740d06652e5fb4d30e6bb6bc3d16dba42e0e9e0
nikhil <nikhil@gmail.com>
2024-04-27 08:08:52 +0530

    add

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,41 +1,27 @@
 // NavBar.js
-import React from 'react';
-import {Link} from 'react-router-dom'
-import './index.css';  // Import specific CSS for the NavBar
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './index.css'; // Import specific CSS for the NavBar
 
 const Header = () => {
-    return (
-        <nav className="navbar">
-            <div className="navbar-container">
-            <Link to="/" >
-            <h1 className="logo">JobApp</h1>
-            </Link>
-               
-                <ul className="nav-links">
-                    <li>
-                        <Link to="/">
-                        Home 
-                        </Link>
-                    </li> 
-                    <li>
-                        <Link to="/jobs">
-                        Jobs 
-                        </Link>
-                    </li> 
-                    <li>
-                        <Link to="/about">
-                        About us
-                        </Link>
-                    </li> 
-                    <li>
-                        <Link to="/contact">
-                        Contact us
-                        </Link>
-                    </li> 
-                </ul>
-            </div>
-        </nav>
-    );
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
+
+  return (
+    <nav className={`navbar ${window.pageYOffset > 20 ? 'navbar-scrolling' : ''}`}>
+      <div className="navbar-container">
+        <Link to="/" className="logo">JobApp</Link>
+    
+        <ul className={`nav-links ${isMobileMenuOpen ? 'mobile-nav-links active' : 'mobile-nav-links'}`}>
+          <li><Link to="/" onClick={() => setIsMobileMenuOpen(false)}>Home</Link></li>
+          <li><Link to="/jobs" onClick={() => setIsMobileMenuOpen(false)}>Jobs</Link></li>
+          <li><Link to="/about" onClick={() => setIsMobileMenuOpen(false)}>About us</Link></li>
+          <li><Link to="/contact" onClick={() => setIsMobileMenuOpen(false)}>Contact us</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
